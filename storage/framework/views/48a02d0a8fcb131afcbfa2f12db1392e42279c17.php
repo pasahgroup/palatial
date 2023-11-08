d63da9caf3aa3bc019cc8d11627baeccfe23db7e
pasahgroup <info@pasah.net>
2023-11-08 12:19:47 +0300

    08 Nov 2023

--- storage/framework/views/48a02d0a8fcb131afcbfa2f12db1392e42279c17.php
+++ storage/framework/views/48a02d0a8fcb131afcbfa2f12db1392e42279c17.php
@@ -10,7 +10,7 @@
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
-      <div class="container-fluid">
+      <div class="row container-fluid">
         <div class="mb-2">
           <div class="col-sm-6">
             <h1>Tailor Made Page</h1>
