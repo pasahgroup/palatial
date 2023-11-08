0dc6b90463c6407ff9cbde4b790bd3687502764b
pasahgroup <info@pasah.net>
2023-11-08 14:12:32 +0300

    08 Nov 2023

--- storage/framework/views/84c24795bc7c5dd56a4f69051d0a9b6c78ae47ce.php
+++ storage/framework/views/84c24795bc7c5dd56a4f69051d0a9b6c78ae47ce.php
@@ -11,7 +11,7 @@
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <div class="container-fluid">
-        <div class="mb-2">
+        <div class="row mb-2">
           <div class="col-sm-6">
             <h1>Enquiry Page</h1>
           </div>
