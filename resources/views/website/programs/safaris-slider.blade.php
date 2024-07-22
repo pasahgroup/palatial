2313e1b25e87bdfa250d4ce0a7369c4d5594bd6c
pasahgroup <info@pasah.net>
2024-07-22 10:04:15 +0300

    22 july 2024

--- resources/views/website/programs/safaris-slider.blade.php
+++ resources/views/website/programs/safaris-slider.blade.php
@@ -13,7 +13,7 @@
     <div class="container">
           <!-- Start of Image filled from slider -->
              <div class="package-list-wrap ">
-                 <img src="{{URL::asset('/storage/uploads/'.$sliders->attachment) }}" class="" alt="det-img" style="min-height: 35vh !important;max-height:50vh !important;background-size:100% 100%;width: 100%;">
+                 <img src="{{URL::asset('/storage/uploads/'.$sliders->attachment) }}" class="" alt="det-img" style="min-height: 50vh !important;max-height:50vh !important;background-size:100% 100%;width: 100%;">
                  
                 <div class="package-list-content">
                     <p class="package-list-duration"> <span
@@ -43,94 +43,101 @@
 </br>
       <!-- End of Image filled from slider -->
      
-      
-        <div class="masonry-item">
-            <div class="row">
-            <div class="col-md-12" data-aos="fade-up">
-                 
-                  <div class="col-md-12 text-center">
-         <h3 style="color:#b76b0b;" class="">More: {{$title}}</b></h3>
-                    </div>
-            </div>
-        </div>
-            <!-- item -->
-            <div class="row" data-aos="fade-up"> 
-            @foreach ($safaris as $safari)
-                 <div class="col-lg-4 col-md-4">
-                        <div class="single_blog listing-shot">                         
-                               
-                                <div class="listing-shot-img">
-                                    <div class="blog_image">
-                                    <img src="{{URL::asset('/storage/uploads/'.$safari->attachment) }}" class="img-responsive" alt="{{  $safari->tour_name }}" style="height:250px;width:100%;">
-                                    </div>
-                                </div>
-                                <div class="">
-                               
-                                      <h4 class="text-center demo-3"> <b style="color:#b76b0b" title="{{$safari->tour_name}}" style="color:red">{{$safari->tour_name}}</b>
-                                                                             </h4>
-                                </div>
-                               
-                            <div class="blog-text">       
-                            <div class="row">                                                          
-                                    <div class="col-md-6 col-sm-12 col-xs-6 booking-btn" style="border-right:1px solid rgba(71,85,95,.11);font-size:14px;">
-                                             <strong>{{ $safari->days }} Days, {{ $safari->days -1 }} Nights</strong>
-                                        </div>                                           
-                                        <div class="col-md-6 col-sm-12 col-xs-6 booking-btn" style="font-size:14px;">
-                                        <span class="text-white"><strong>From ${{number_format($safari->price),2 }}</b>  </strong>
-                                           </span>
-
-                                         </div> 
-                                    </div> 
-                                    
-                                      <div class="col-md-12 col-sm-12 col-xs-12 text-left booking-btn-gray">
-                                      
-                                    
-                                               <div class="row">                                            
-                                              <div class="col-md-6 col-sm-6 col-xs-6" style="border-right:1px solid rgba(255,255,0,.5);font-size:17px;">
-                                                   <span>Physical Rating:</span>
-                                                </div>                                          
-
-                                               <div class="col-md-5 col-sm-5 col-xs-5" style="font-size:17px;">
-                                                   <strong>{{ $safari->physical_rating }}</strong>
-                                                </div>
-                                               </div>
-                                                <div class="row">
-                                                                                             
-                                              <div class="col-md-6 col-sm-6 col-xs-6" style="border-right:1px solid rgba(255,255,0,.5);font-size:17px;">
-                                                   <span>Tour Category:</span>
-                                                </div>  
-                            
-                                           
-                                               <div class="col-md-6 col-sm-6 col-xs-6" style="font-size:17px;">
-                                                   <strong>{{ $safari->category }}</strong>
-                                                </div>  
-                                            </div>  
-                                                  <div class="row">
-                                               <div class="col-md-6 col-sm-6 col-xs-6" style="border-right:1px solid rgba(255,255,0,.5);font-size:17px;">
-                                                   <span>Tour Code:</span>
-                                                </div> 
-                          
-                                               <div class="col-md-6 col-sm-6 col-xs-6" style="font-size:17px;">
-                                                   <strong>{{ $safari->tour_code }}</strong>
-                                                </div>  
-                                            </div>                                           
-                                             </div>
-                               
-                                  <div class="row">
-                                  <div class="col-md-12 col-sm-12 col-xs-12 text-right booking-btn booking-tourPadding">
-                                    <div class="">
-                                        <a href="{{ route('safaris.show',$safari->id) }}" class="booking-btn-green text-center"><b>View More</b></a>
-                                    </div>  
-                                      </div>  
-                                      </div>                         
-                                </div>
-                          
-                        </div>
-                    </div>                 
-            @endforeach
-        </div>
-</div>
+     
     </div>
 </section>
+
+
+  <section class="featured-properties-area section-padding-100-50" style="padding-bottom:0px;">
+    <div class="container" style="padding-bottom:0px;">  
+            
+        <h2 class="section-title position-relative text-uppercase mx-xl-5 mb-4"><span class="bg-secondary pr-3 section-heading wow fadeInUp" style="color:#b76b0b;"> {{$title ?? ''}} Tours</span></h2>
+
+      
+      <div class="px-xl-5 pb-3">     
+<div class="col-md-12 col-sm-12 pb-1" style="background-color:#4c7149">
+  
+   
+                                
+                                            @foreach ($safaris as $safari)
+
+ <div class="col-lg-3 col-md-3">
+    
+       <h5><b style="background:">{{$safari->sales_header}}</b></h5>
+            <div class="col-lg-12 col-md-12 col-sm-12 pb-1" style="background-color:#f3f4f4">
+
+<div class="single_blog listing-shot">
+                <div class="product-item bg-light mb-4">
+                   <div class="listing-shot-img">
+                                                                           
+                                                               
+
+                    <div class="product-img position-relative overflow-hidden">
+                        <img class="img-fluid w-100" src="{{ URL::asset('/storage/uploads/'.$safari->attachment) }}" alt="" style="height:250px;">
+                        <div class="product-action">
+                            <a class="btn btn-outline-dark btn-squarex" href="/safaris"><i class="fa fa-search">  {{ $safari->tour_name }}</i></a>
+                        </div>
+                    </div>
+
+                    <div class="text-center">
+                        <a class="h6 text-decoration-none text-truncate" href="/safaris">{{ $safari->tour_name }}</a>                      
+                    </div>
+                </div>
+                 </div>
+
+  <b>  <div class="col-md-5 col-sm-5 col-xs-5" style="border-right:0px solid rgba(71,85,95,.11);height:0px; ">
+                                                                                         <strong style="background-color:;">{{ $safari->days }} Days, {{ $safari->days -1 }} Nights:</strong>
+                                                                                 </div></b>
+
+ <b class="float-right">  
+  <div class="header-btn">
+                                                                                <span class="text-danger" style="font-size:17px"><strong>${{ number_format($safari->price),2 }} </strong>
+                                                                                     </span>
+                                                                                    
+
+                                                                                 </div></b>
+
+
+
+
+
+               </div>
+
+
+<div>Physical Rating:          <strong class="float-right">{{$safari->physical_rating }}</strong></div>
+<div>Tour Category:          <strong class="float-right">{{$safari->category }}</strong></div>
+<div>Physical Rating:          <strong class="float-right">{{$safari->tour_code }}</strong></div>
+
+
+<hr>
+                                                                        <div class="text-right">
+                                                                                 
+
+                                                                                                 @if($safari->category=="Private") 
+                            <a href="{{ route('safaris.show',$safari->id) }}" class="booking-btn text-center"><b>View More</b></a>
+                               @endif 
+
+                                           @if($safari->category=="Group") 
+                            <a href="{{ route('grouptour.show',$safari->id) }}" class="booking-btn text-center"><b>View More</b></a>
+                               @endif
+                                                                        </div>
+            </div>
+
+</div>
+
+
+                                @endforeach
+
+
+
+
+
+        </div>  
+
+    </div>
+</div>
+</section>
+
+
 <!-- End-Package-Section -->
 @endsection
